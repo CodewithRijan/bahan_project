b27d47fc14fd697c7a98ac230746615d2b183591
SunyaCore <codewithrijan21st@gmail.com>
2025-11-11 08:53:57 +0000

    [3]: Added basic login and sign up forms

--- crowdsource_app/urls.py
+++ crowdsource_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name="crowdsource_app"
 urlpatterns = [
-    path('',views.home, name="home")
+    path('',views.home, name="home"),
+    path('sign_up/',views.SignUpView.as_view(),name="signup"),
+    path('report/',views.report,name="report")
 ]
