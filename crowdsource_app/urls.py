d7eab9591a4378cf53a8666dd8a8eb5a2788b325
SunyaCore <codewithrijan21st@gmail.com>
2025-11-12 14:43:29 +0000

    Switched authenticatio to seperate accounts app

--- crowdsource_app/urls.py
+++ crowdsource_app/urls.py
@@ -5,6 +5,5 @@ from . import views
 app_name="crowdsource_app"
 urlpatterns = [
     path('',views.home, name="home"),
-    path('sign_up/',views.SignUpView.as_view(),name="signup"),
     path('report/',views.report,name="report")
 ]
